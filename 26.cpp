c188a7e63312a1bc23925f199b1c777bba8f7608
SashkoSt <oleksandrstefaniyk@gmail.com>
2024-09-26 15:14:40 +0300

    Add project files.

---
+++ 26.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <sstream>
+#include <Windows.h>
+using namespace std;
+
+bool isValidIpAddress(string ip) {
+    stringstream s(ip);
+    string seg;
+    int num, dot = 0;
+
+
+    while (getline(s, seg, '.')) {
+        dot++;
+
+        if (seg.empty() || seg.size() > 3) return false;
+
+        for (char c : seg) {
+            if (!isdigit(c)) return false;
+        }
+
+        num = stoi(seg);
+
+        if (num < 0 || num > 255) return false;
+    }
+
+    return dot == 4;
+}
+
+int main() {
+    SetConsoleCP(1251); 
+    SetConsoleOutputCP(1251); 
+    string ip;
+    cout << "Введіть IP-адресу: ";
+    cin >> ip;
+
+    if (isValidIpAddress(ip)) {
+        cout << "Коректна IP-адреса" << endl;
+    }
+    else {
+        cout << "Некоректна IP-адреса" << endl;
+    }
+
+    return 0;
+}
